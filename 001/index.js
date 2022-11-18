15d77d34c2d39ee8dfd4050e11f6388cae2c02bd
yuko <hero707@gmail.com>
2022-11-18 10:52:33 +0900

    first

---
+++ 001/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+app.use(express.urlencoded({extended: true})) 
+const bodyParser= require('body-parser')
+app.use(bodyParser.urlencoded({extended: true})) 
+
+const MongoClient = require('mongodb').MongoClient
+
+var db;
+MongoClient.connect("mongodb+srv://labos:hallym@cluster0.vvtp274.mongodb.net/?retryWrites=true&w=majority", function(err, client){
+  if (err) return console.log(err)
+  db = client.db('nodejs');
+  db.collection('login').insertOne({email: 'apple@hallym.ac.kr', password: "apple"}, function(err, result){
+    console.log("save complete...");
+  });
+
+
+  app.listen(8080, function() {
+    console.log('listening on 8080')
+  })
+})
+
+
+app.get('/', function(req, res) { 
+  res.sendFile(__dirname +'/index.html')
+  })
+
+app.get('/write', function(req, res) { 
+    res.sendFile(__dirname +'/write.html')
+  })
+
+app.post('/add', function(req, res){
+    console.log(req.body);
+    res.send('complete....')
+  });
+  
